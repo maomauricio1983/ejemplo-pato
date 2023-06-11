e60ba7c7e5a246d6b973cccfe9fc9ef2339f89c0
Andres220510 <maomauriciomunoz@gmail.com>
2023-06-11 13:12:23 -0500

    mi primer commit

---
+++ src/main/java/com/pato/app/ejemplopato/EjemploPatoApplication.java
@@ -0,0 +1,13 @@
+package com.pato.app.ejemplopato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploPatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjemploPatoApplication.class, args);
+    }
+
+}
